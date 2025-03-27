a78956130f8145b75673fb37d9ab80aedbf398ea
Ratikanta Rout <111556535+Ratikant-rout@users.noreply.github.com>
2025-03-27 21:43:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import AnalyzeCode from "./AnalyzeCode";
 
 function App() {
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      {/* ✅ Added a container with proper styling */}
       <AnalyzeCode />
     </div>
   );
